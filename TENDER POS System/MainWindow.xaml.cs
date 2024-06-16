6b642b15a0ea260c0cb868bedaeffcff6f2975d6
PaperKurane <omginlol@gmail.com>
2024-06-16 21:49:26 +0800

    Images are ok so far

--- TENDER POS System/MainWindow.xaml.cs
+++ TENDER POS System/MainWindow.xaml.cs
@@ -20,14 +20,67 @@ namespace TENDER_POS_System
     /// </summary>
     public partial class MainWindow : Window
     {
+        TenderConnDataContext _dbConn = null;
+
         public MainWindow()
         {
             InitializeComponent();
+
+            _dbConn = new TenderConnDataContext(Properties.Settings.Default.TenderConnectionString);
+
+            LoadMenuItems("1"); // 1 is defaulted to ricemeals
         }
 
-        private void lbItem1_TextChanged(object sender, TextChangedEventArgs e)
+        private void LoadMenuItems(string categoryID)
         {
+            List<MenuItem> menuItems = GetMenuItems(categoryID);
+            BindMenuItems(menuItems);
+        }
 
+        private List<MenuItem> GetMenuItems(string categoryID)
+        {
+            var items = from item in _dbConn.Menu_Items
+                        where item.Category_ID == categoryID
+                        select new MenuItem
+                        {
+                            Item_ID = item.Item_ID,
+                            Item_Name = item.Item_Name,
+                            Category_ID = item.Category_ID,
+                            Item_Price = item.Item_Price,
+                            Item_Image = item.Item_Image
+                        };
+
+            return items.ToList();
+        }
+
+        private void BindMenuItems(List<MenuItem> menuItems)
+        {
+            for (int x = 0; x < menuItems.Count && x < 12; x++)
+            {
+                var item = menuItems[x];
+
+                // Set image source
+                Image img = (Image)FindName("imgItem" + (x + 1));
+                if (img != null)
+                {
+                    try
+                    {
+                        string imagePath = $"pack://application:,,,/Resources/Menu Items/{item.Item_Image}";
+                        img.Source = new BitmapImage(new Uri(imagePath));
+                    }
+                    catch
+                    {
+                        img.Source = new BitmapImage(new Uri("pack://application:,,,/Resources/Menu Items/defaultimg.png"));
+                    }
+                }
+
+                // Set item name
+                TextBox txt = (TextBox)FindName("lbItem" + (x + 1));
+                if (txt != null)
+                {
+                    txt.Text = item.Item_Name;
+                }
+            }
         }
 
         private void imgItem1_MouseDown(object sender, MouseButtonEventArgs e)
---
+++ TENDER POS System/MenuItem.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TENDER_POS_System
+{
+    public class MenuItem
+    {
+        public string Item_ID { get; set; }
+        public string Item_Name { get; set; }
+        public string Category_ID { get; set; }
+        public decimal Item_Price { get; set; }
+        public string Item_Image { get; set; }
+    }
+}
