dc857d37d5a1cf3ac36427fdcbe1c84010d20d5c
PaperKurane <omginlol@gmail.com>
2024-06-18 17:42:22 +0800

    Fucking kill me rn please ong im begigng you true i will win 5050 ff rm

--- TENDER POS System/CameraWindow.xaml.cs
+++ TENDER POS System/CameraWindow.xaml.cs
@@ -1,9 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.IO;
 using System.Globalization;
 using System.Linq;
-using System.Drawing;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
